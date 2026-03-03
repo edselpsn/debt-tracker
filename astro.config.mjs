a7dc42ee129540d201df6a5a464af14e3e193f01
Edsel Pisueña <82765610+edselpsn@users.noreply.github.com>
2026-03-04 00:08:17 +0800

    feat: setup PWA and server adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,45 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
+import vitePwa from '@vite-pwa/astro'
+import node from '@astrojs/node'
 
-// https://astro.build/config
 export default defineConfig({
+  output: 'server',
+
+  adapter: node({
+  mode: 'standalone'
+  }),  
+
   vite: {
     plugins: [tailwindcss()]
   },
-  output: 'server'
-});
+
+  integrations: [
+    vitePwa({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Debt Tracker',
+        short_name: 'DebtTracker',
+        description: 'Couple Debt Payoff Tracker',
+        theme_color: '#111827',
+        background_color: '#111827',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+})
